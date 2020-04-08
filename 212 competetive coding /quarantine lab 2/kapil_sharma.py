8f46413427be10d601a791c0cb1b315ad83ae937
REGATTE <regatte@REGATTEs-MacBook-Pro.local>
2020-04-08 14:24:22 +0530

    git commit

--- 212 competetive coding /quarantine lab 2/kapil_sharma.py
+++ 212 competetive coding /quarantine lab 2/kapil_sharma.py
@@ -24,26 +24,25 @@
 #2
 
 
-
-N = int(input())
+Num = int(input())
 def check(data,i,j):
-    for r in range(i,N):
-        if(data[r][j]==0):
-            return False
-    for c in range(j,N):
-        if(data[i][c]==0):
-            return False
-    return True
+        for r in range(i,Num):
+            if(data[r][j]==0):
+                return False
+        for c in range(j,Num):
+            if(data[i][c]==0):
+                return False
+        return True
 
-data = [0]*N
-for i in range(N):
+data = [0]*Num
+for i in range(Num):
+
     data[i] = list(map(int,input().split()))
 
-count = 0
+    count = 0
 
-for i in range(N):
-    for j in range(N):
-        if(data[i][j] and check(data,i,j)):
-            count+=1
-
+for i in range(Num):
+        for j in range(Num):
+            if(data[i][j] and check(data,i,j)):
+                count+=1
 print(count)
