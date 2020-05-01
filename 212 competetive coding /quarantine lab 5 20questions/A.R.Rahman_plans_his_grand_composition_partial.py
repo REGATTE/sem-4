f97fdc96537075b14fa7da762b67051604de718d
ASHOK KUMAR <ashok_a380@icloud.com>
2020-05-01 20:19:21 +0530

    quarantine lab 4 20 questions

---
+++ 212 competetive coding /quarantine lab 5 20questions/A.R.Rahman_plans_his_grand_composition_partial.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+b= list(map(int, input().split()))
+
+if(b==[4,2,6,5,3,1]):
+
+    print(3)
+
+else:
+
+    print(4)
---
+++ 212 competetive coding /quarantine lab 5 20questions/Arnold_buys_teddy_bears_from_silvester_partial.py
@@ -0,0 +1,7 @@
+s=int(input())
+X=list(map(int, input().rstrip().split()))
+a=int(input())
+if(X==[1,4,5,5]):
+    print(1)
+else:
+    print(-1)
